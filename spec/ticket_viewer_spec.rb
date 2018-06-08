97aa71c128b8a73c404d30a23063311fa07c7271
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-08 16:43:25 +1000

    write basic bahaviours

---
+++ spec/ticket_viewer_spec.rb
@@ -0,0 +1,18 @@
+describe 'Zendesk Ticket Viewer' do
+
+  it 'should connect to Zendesk API'
+
+  it 'should request all the tickets for the account'
+
+  it 'should display tickets in a list'
+
+  it 'should display individual ticket details'
+
+  it 'should page through tickets when more than 25 are returned'
+
+# * Connect to the Zendesk API
+# * Request all the tickets for your account
+# * Display them in a list
+# * Display individual ticket details
+# * Page through tickets when more than 25 are returned
+end
