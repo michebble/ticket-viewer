4dd6fc8c235042ca54584a4d97a535378d5ffc57
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-10 10:23:22 +1000

    write api connect tickets behaviour

--- spec/api_connect_spec.rb
+++ spec/api_connect_spec.rb
@@ -1,10 +1,13 @@
 require 'api_connect'
 
 describe 'api_connect' do
+  let(:api_connect) { Api_connect.new }
 
   it 'should connect to Zendesk API' do
-    api_connect = Api_connect.new
     expect(api_connect.code).to be_truthy
   end
 
+  it 'should request all the tickets for the account' do
+    expect(api_connect.tickets).to be_a Array
+  end
 end
