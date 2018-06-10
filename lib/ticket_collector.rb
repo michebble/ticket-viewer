0aea6139fd942b66a5d8147593ddee5a192a0a0b
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-10 10:33:29 +1000

    rename api connect to ticket colletor and refactor

--- lib/ticket_collector.rb
+++ lib/ticket_collector.rb
@@ -2,29 +2,26 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-class Api_connect
+class Ticket_collector
   attr_reader :code
   attr_reader :tickets
 
   def initialize
+    response = call_api
+    @code = response.code 
+    body = JSON.parse(response.body)
+    @tickets = body["tickets"]
+  end
+
+  def call_api
     uri = URI.parse("https://michebble.zendesk.com/api/v2/tickets.json")
     request = Net::HTTP::Get.new(uri)
     request.basic_auth("hebble.michael@gmail.com", "happypath")
-
     req_options = {
       use_ssl: uri.scheme == "https"
     }
-
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-
-    @code = response.code 
-
-    @tickets = JSON.parse(response.body)["tickets"]
-  end
-
-  def parse_body
-
   end
 end
