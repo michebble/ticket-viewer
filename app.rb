6b361a2cb7ba5ac504a9c857b73de1f3a19e2165
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-11 16:37:30 +1000

    create basic app

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require_relative 'lib/ticket_collector'
 require_relative 'lib/ticket_viewer'
 require_relative 'lib/interface'
-require 'pry'
+
 
 tickets = Ticket_collector.new.tickets
 viewer = Ticket_viewer.new(tickets)
 interface = Interface.new(viewer)
+puts 'Welcome to the Zendesk ticket viewer'
+interface.execute('list')
+loop do
+  input = gets.downcase.chomp
+  if input == 'quit'
+    break
+  else
+    interface.execute(input)
+  end
+end
+
+puts "\nThank you for using the ticket viewer"
 
 
-
-binding.pry
