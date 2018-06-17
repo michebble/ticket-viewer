f8a3088c9974faaca87be24f2a85e414504e8b9e
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-17 17:00:45 +1000

    improve ticket_collector error handling

--- lib/ticket_collector.rb
+++ lib/ticket_collector.rb
@@ -7,15 +7,18 @@ class Ticket_collector
   def initialize(url, basic_auth)
     response = call_api(url, basic_auth)
     @code = response.code
-    body = response.parsed_response
-    @tickets = body["tickets"]
+    if code == 200
+      @tickets = response.parsed_response["tickets"] 
+    else 
+      raise "ConectionError"
+    end 
   end
 
   def call_api(url, basic_auth)
     begin
       HTTParty.get(url, basic_auth: basic_auth)
-    rescue
-      nil
+    rescue StandardError
+      { code => false }
     end
   end
 end
