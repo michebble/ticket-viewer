96256ac4212c499edb2a4d32c96b65fce2e2b691
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-17 17:57:58 +1000

    broke testing, collect all pages

--- lib/ticket_collector.rb
+++ lib/ticket_collector.rb
@@ -1,21 +1,33 @@
 require 'httparty'
+require 'pry'
 
 class Ticket_collector
   attr_reader :code
   attr_reader :tickets
 
   def initialize(basic_auth)
-    url = "https://michebble.zendesk.com/api/v2/tickets.json"
-    response = call_api(url, basic_auth)
-    @code = response.code
-    code == 200 ? @tickets = response.parsed_response["tickets"] : raise("ConectionError") 
+    @basic_auth = basic_auth
+    @tickets = return_all_tickets
   end
 
-  def call_api(url, basic_auth)
+  def call_api(page_num)
+    url = "https://michebble.zendesk.com/api/v2/tickets.json?page=" + page_num.to_s
     begin
-      HTTParty.get(url, basic_auth: basic_auth)
+      HTTParty.get(url, basic_auth: @basic_auth)
     rescue StandardError
       { code => false }
     end
+  end
+
+  def return_all_tickets
+    collected_tickets = Array.new
+    page_num = 1
+    loop do
+      response = call_api(page_num)
+      binding.pry
+      response.code == 200 ? collected_tickets.push(response["tickets"]).flatten! : raise("ConectionError")
+      collected_tickets.length == response["count"] ? break : page_num += 1
+    end
+    collected_tickets
   end
 end
